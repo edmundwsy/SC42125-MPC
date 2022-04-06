75fa094e5d307ece204954452e7bbd755ce75029
edmundwsy <edmundwsy@outlook.com>
2022-04-06 17:18:39 +0200

    remove tensorflow

--- high_mpc/common/util.py
+++ high_mpc/common/util.py
@@ -1,7 +1,6 @@
 import os
 import random
 import numpy as np
-import tensorflow as tf
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 #
@@ -14,36 +13,36 @@ def get_dir(path):
 def set_global_seed(seed, env=None):
     random.seed(seed)
     np.random.seed(seed)
-    tf.random.set_seed(seed)
+    # tf.random.set_seed(seed)
     if env is not None:
         env.seed(seed)
 
-def tf_config_gpu_memory():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+# def tf_config_gpu_memory():
+#     gpus = tf.config.experimental.list_physical_devices('GPU')
+#     if gpus:
+#         try:
+#             # Currently, memory growth needs to be the same across GPUs
+#             for gpu in gpus:
+#                 tf.config.experimental.set_memory_growth(gpu, True)
+#                 logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+#                 print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#         except RuntimeError as e:
+#             # Memory growth must be set before GPUs have been initialized
+#             print(e)
 
-def tf_config_gpu_memory_02():
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-        try:
-            tf.config.experimental.set_virtual_device_configuration(
-                gpus[0],
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
-            print(e)
+# def tf_config_gpu_memory_02():
+#     gpus = tf.config.experimental.list_physical_devices('GPU')
+#     if gpus:
+#         # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+#         try:
+#             tf.config.experimental.set_virtual_device_configuration(
+#                 gpus[0],
+#                 [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+#             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+#             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+#         except RuntimeError as e:
+#             # Virtual devices must be set before GPUs have been initialized
+#             print(e)
 
 #
 def merge_dicts(*dicts):
